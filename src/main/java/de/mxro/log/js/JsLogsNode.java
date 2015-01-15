0777110b5ae9ad9f709751bf1227618cab05da0f
mxro <mxro@nowhere.com>
2015-01-16 07:54:25 +1100

    Implementing logs-js Autocommit Fri Jan 16 07:54:25 EST 2015

--- src/main/java/de/mxro/log/js/JsLogsNode.java
+++ src/main/java/de/mxro/log/js/JsLogsNode.java
@@ -4,12 +4,23 @@ import org.timepedia.exporter.client.Export;
 import org.timepedia.exporter.client.Exportable;
 import org.timepedia.exporter.client.NoExport;
 
+import de.mxro.async.log.LogsCommon;
 import de.mxro.async.properties.PropertyNode;
 
 @Export
 public class JsLogsNode implements Exportable {
 
     private PropertyNode node;
+
+    @Export
+    public String string(final String id, final String message) {
+        return this.node.record(LogsCommon.string(id, message)).get();
+    }
+
+    @Export
+    public String render() {
+        return this.node.render().get();
+    }
 
     @NoExport
     public static final JsLogsNode wrap(final PropertyNode node) {
